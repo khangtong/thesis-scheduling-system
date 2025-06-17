aa1dec0cdaca15c6ac25d3fa9a4a00d57ca84e86
Khangtong <duykhangtong99@gmail.com>
2025-06-17 09:26:48 +0700

    Initial commit

---
+++ frontend/src/app/ui/logo.tsx
@@ -0,0 +1,13 @@
+import { lusitana } from './fonts';
+import { AcademicCapIcon } from '@heroicons/react/24/outline';
+
+export default function Logo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      <AcademicCapIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">Thesis Scheduling System</p>
+    </div>
+  );
+}
